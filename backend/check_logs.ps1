e170e5f77137429ec95bcbf935d8380b44e93ec3
Pratik Parmar <parmarpratik9642@outlook.com>
2026-04-07 16:45:47 +0530

    chore: remove files with hardcoded credentials

--- backend/check_logs.ps1
+++
@@ -1,7 +0,0 @@
-$connectionString = "Server=192.250.231.31;Database=m2websol_apptdb;User=m2websol_apptusr;Password=omiBSR&ommrt32#0;"
-# Using a simple mysql command if available, or just outputting a message if we can't.
-# Since I'm on Windows, I'll try to find a way to run a one-off query if possible, 
-# but simply checking the code is faster.
-Write-Host "Checking EmailLogs table..."
-# I'll just check if there are any recent logs here if I can, but I don't have a direct mysql client easy to use.
-# I'll assume the user needs me to switch to SMTP or fix the registration.
